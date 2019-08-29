1616302b91fe00662ef22ebdb90662c4bf499318
Daniel Marsh-Patrick <d.s.marsh@gmail.com>
2019-08-29 18:01:06 +1200

    #1: Updated help URL with bit.ly link and turned off dev mode flag

--- src/constants.ts
+++ src/constants.ts
@@ -2,5 +2,5 @@
     export const VISUAL_NAME: string = 'chart.xkcd for Power BI';
     export const VISUAL_VERSION: string = '1.0.0.0';
     export const VISUAL_CHARTXKCD_VERSION: string = '1.0.10';
-    export const VISUAL_USAGE_URL: string = 'https://github.com/dm-p/powerbi-visuals-chartxkcd';
-    export const VISUAL_NON_PRODUCTION: boolean = true; /** SET TO `false` FOR RELEASE */
+    export const VISUAL_USAGE_URL: string = 'https://bit.ly/powerbi-xkcd';
+    export const VISUAL_NON_PRODUCTION: boolean = false; /** SET TO `false` FOR RELEASE */
