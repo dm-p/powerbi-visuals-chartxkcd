b1f545715933ac900bec266b372d8cfe2cbfb4c7
Daniel Marsh-Patrick <d.s.marsh@gmail.com>
2019-08-29 15:24:31 +1200

    Updated constants to reflect correct version of chart.xkcd

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 /** Visual  */
     export const VISUAL_NAME: string = 'chart.xkcd for Power BI';
     export const VISUAL_VERSION: string = '1.0.0.0';
-    export const VISUAL_CHARTXKCD_VERSION: string = '1.0.9';
+    export const VISUAL_CHARTXKCD_VERSION: string = '1.0.10';
     export const VISUAL_USAGE_URL: string = 'https://github.com/dm-p/powerbi-visuals-chartxkcd';
     export const VISUAL_NON_PRODUCTION: boolean = true; /** SET TO `false` FOR RELEASE */
