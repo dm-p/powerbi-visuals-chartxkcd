e5384aab9615b1f419ff48ed91eac359d7312a02
Daniel Marsh-Patrick <d.s.marsh@gmail.com>
2019-08-31 12:25:49 +1200

    Updating version number

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 /** Visual  */
     export const VISUAL_NAME: string = 'chart.xkcd for Power BI';
-    export const VISUAL_VERSION: string = '1.0.0.0';
+    export const VISUAL_VERSION: string = '1.0.1.1';
     export const VISUAL_CHARTXKCD_VERSION: string = '1.0.10';
     export const VISUAL_USAGE_URL: string = 'https://bit.ly/powerbi-xkcd';
     export const VISUAL_NON_PRODUCTION: boolean = false; /** SET TO `false` FOR RELEASE */
